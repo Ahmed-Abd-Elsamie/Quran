7d3731666b5dc07eba525943680cfa98845ace23
Ahmed <ahmed.a199810@gmail.com>
2024-04-08 01:29:45 +0200

    empty marks

--- lib/views/saved_marks.dart
+++ lib/views/saved_marks.dart
@@ -17,94 +17,98 @@ class SavedMarks extends StatelessWidget {
       body: Center(
         child: GetBuilder<MarksController>(
           builder: (c) {
-            return _marksController.loading.value
-                ? CircularProgressIndicator()
-                : ListView.builder(
-                    itemCount: _marksController.marksList.length,
-                    itemBuilder: (ctx, index) {
-                      return Container(
-                        margin: EdgeInsets.all(5),
-                        child: TextButton(
-                            style: ButtonStyle(
-                                padding: MaterialStateProperty.all(
-                                    EdgeInsets.only(right: 20, top: 10)),
-                                backgroundColor:
-                                    MaterialStateProperty.all(Colors.white),
-                                elevation: MaterialStateProperty.all(5),
-                                shape: MaterialStateProperty.all<
-                                        RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ))),
+            if (_marksController.loading.value) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (_marksController.marksList.isEmpty) {
+              return Center(
+                child: Text(
+                  "لا توجد علامات مرجعية",
+                  style: TextStyle(fontSize: 17),
+                ),
+              );
+            }
+            return ListView.builder(
+              itemCount: _marksController.marksList.length,
+              itemBuilder: (ctx, index) {
+                return Container(
+                  margin: EdgeInsets.all(5),
+                  child: TextButton(
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                              EdgeInsets.only(right: 20, top: 10)),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          elevation: MaterialStateProperty.all(5),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ))),
+                      onPressed: () {
+                        print(
+                            _marksController.marksList[index].type.toString());
+                        _mainController.changeSurah(
+                            _marksController.marksList[index].page_num);
+                        Get.back();
+                      },
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          TextButton.icon(
                             onPressed: () {
-                              print(_marksController.marksList[index].type
-                                  .toString());
-                              _mainController.changeSurah(
-                                  _marksController.marksList[index].page_num);
-                              Get.back();
+                              _marksController.deleteMark(
+                                  _marksController.marksList[index].id);
                             },
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                TextButton.icon(
-                                  onPressed: () {
-                                    _marksController.deleteMark(
-                                        _marksController.marksList[index].id);
-                                  },
-                                  label: Text("حذف العلامه"),
-                                  icon: Icon(
-                                    Icons.delete_forever,
-                                    color: Colors.brown,
+                            label: Text("حذف العلامه"),
+                            icon: Icon(
+                              Icons.delete_forever,
+                              color: Colors.brown,
+                            ),
+                          ),
+                          new Expanded(
+                            child: new Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 5.0),
+                              child: new Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  new Text(
+                                    _marksController.marksList[index].surah,
+                                    textDirection: TextDirection.rtl,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
                                   ),
-                                ),
-                                new Expanded(
-                                  child: new Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 5.0),
-                                    child: new Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.end,
-                                      children: <Widget>[
-                                        new Text(
+                                  const Padding(
+                                      padding: const EdgeInsets.only(top: 5.0)),
+                                  new Text(
+                                      " صفحه رقم " +
                                           _marksController
-                                              .marksList[index].surah,
-                                          textDirection: TextDirection.rtl,
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 18),
-                                        ),
-                                        const Padding(
-                                            padding: const EdgeInsets.only(
-                                                top: 5.0)),
-                                        new Text(
-                                            " صفحه رقم " +
-                                                _marksController
-                                                    .marksList[index].page_num
-                                                    .toString(),
-                                            textAlign: TextAlign.end,
-                                            textDirection: TextDirection.ltr,
-                                            style: TextStyle(fontSize: 17)),
-                                        new Text(
-                                            _marksController.marksList[index]
-                                                        .type ==
-                                                    1
-                                                ? "علامه القراءه"
-                                                : "علامه الحفظ".toString(),
-                                            textAlign: TextAlign.end,
-                                            textDirection: TextDirection.ltr,
-                                            style: TextStyle(fontSize: 17)),
-                                        const Padding(
-                                            padding: const EdgeInsets.only(
-                                                top: 5.0)),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            )),
-                      );
-                    },
-                  );
+                                              .marksList[index].page_num
+                                              .toString(),
+                                      textAlign: TextAlign.end,
+                                      textDirection: TextDirection.ltr,
+                                      style: TextStyle(fontSize: 17)),
+                                  new Text(
+                                      _marksController.marksList[index].type ==
+                                              1
+                                          ? "علامه القراءه"
+                                          : "علامه الحفظ".toString(),
+                                      textAlign: TextAlign.end,
+                                      textDirection: TextDirection.ltr,
+                                      style: TextStyle(fontSize: 17)),
+                                  const Padding(
+                                      padding: const EdgeInsets.only(top: 5.0)),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                );
+              },
+            );
           },
         ),
       ),
